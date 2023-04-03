14c8d3a8ea7a5f6f77470ce6ba9086f1c798e8f2
刘中奇 <Liuzq811@163.com>
2023-04-03 16:02:12 +0800

    styled-components use

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import type { NavigateFunction } from 'react-router-dom'
+import styled from 'styled-components'
 import { Button } from 'antd'
+
+const Title = styled.div`
+	font-size: 30px;
+	text-align: center;
+	color: #0ff;
+	.h2-text {
+		color: #ff0;
+	}
+`
+const Title2 = styled.h1`
+	font-size: 30px;
+	text-align: center;
+	color: #f0f;
+	font-weight: 500;
+`
 
 const Login: React.FC = () => {
 	const Nav: NavigateFunction = useNavigate()
@@ -12,7 +28,11 @@ const Login: React.FC = () => {
 
 	return (
 		<div className='login-page'>
-			login-page
+			<Title>
+				login-page
+				<h2 className='h2-text'>hello h2 text</h2>
+				<Title2>Title2</Title2>
+			</Title>
 			<br />
 			<Button onClick={jump} type='primary'>
 				go to dashboard
